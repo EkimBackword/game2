1e31e6ed0e045f40b402cec06cebe0e28be241aa
Жарков Михаил <zharkov@abcreg.ru>
2019-11-20 11:58:29 +0300

    appVersion

--- frontend/src/environments/version.ts
+++ frontend/src/environments/version.ts
@@ -1,4 +1,4 @@
 // This file was automatically generated
 // !!! Версию изменять только в файле package.json !!!
-export const appVersionFull = '1.3.1+build.20191119.135314.UTC';
-export const appVersion = '1.3.1';
+export const appVersionFull = '1.3.3+build.20191120.064519.UTC';
+export const appVersion = '1.3.3';
